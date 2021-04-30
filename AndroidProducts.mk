0f360cab2fdca4c765c453d9bc45fb9fcc9a0f2c
PrajjuS <theprajjus@gmail.com>
2021-04-30 18:36:49 +0000

    vince: sakura adapt
    Change-Id: Id7fa81aea154aa39ed3fa64953eb5e0c5bba8238
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,4 @@
+
 #
 # Copyright (C) 2017-2019 The LineageOS Project
 #
@@ -14,9 +15,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_vince.mk
+    $(LOCAL_DIR)/lineage_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_vince-user \
-    derp_vince-userdebug \
-    derp_vince-eng
+    lineage_vince-user \
+    lineage_vince-userdebug \
+    lineage_vince-eng
