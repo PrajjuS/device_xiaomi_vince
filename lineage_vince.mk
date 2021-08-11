8c2561a2f31e9c628a58b2158b6dce91591a8050
PrajjuS <theprajjus@gmail.com>
2021-08-11 22:06:12 +0530

    vince: Update to Redfin FP
    Change-Id: I797604e78f63725125965b67aac2eb1a4cb5f28f
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- lineage_vince.mk
+++ lineage_vince.mk
@@ -44,7 +44,8 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys"
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
+
