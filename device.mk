39b3e8ebe0899a9a6e059f5a29fcba999b7e0833
ZeeLog <Danjel.Zammit@gmail.com>
2022-03-08 21:11:54 +0530

    vince: Build vendor variant of vendor.display.config@1.0
    - Fixes QSEECOMD: : Init dlopen(libsecureui.so, RLTD_NOW) is failed.... dlopen failed: library "vendor.display.config@1.0.so" not found: needed by /vendor/lib64/libsecureui.so in namespace (default)
    
    Change-Id: If8d64875a130583d105d19c40b2cfb058c6241df
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- device.mk
+++ device.mk
@@ -169,6 +169,7 @@ PRODUCT_PACKAGES += \
     Snap \
     android.frameworks.displayservice@1.0.vendor
 
+
 # Consumerir
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
@@ -193,11 +194,12 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
     libdisplayconfig \
-	libvulkan \
+    libvulkan \
     liboverlay \
     libqdMetaData.system \
     libgenlock \
-    libtinyxml
+    libtinyxml \
+    vendor.display.config@1.0.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
