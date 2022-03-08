65c7908e322c85ab9a556c2c764e4a7840296719
PrajjuS <theprajjus@gmail.com>
2022-03-08 20:40:58 +0530

    vince: Update Redfin FP to March
    Change-Id: Id2636a424fdc0d3bb793b8954829726e275720cb
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- aosp_vince.mk
+++ aosp_vince.mk
@@ -51,8 +51,8 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220205.002/8010174:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SP2A.220305.012/8177914:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220205.002 8010174 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 12 SP2A.220305.012 8177914 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
