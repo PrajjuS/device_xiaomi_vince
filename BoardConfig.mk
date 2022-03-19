f38b4fe2f2421dd4ac21c62419861062b5d00c3a
PrajjuS <theprajjus@gmail.com>
2022-03-19 14:15:29 +0530

    vince: Switch to Cosmic clang
    Change-Id: Ibc24e7b66ecbc2652aff595472f9c67674a226c3
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,9 +59,10 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/vince
 TARGET_KERNEL_VERSION := 4.9
 
-# Use proton-clang
+# Clang
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CLANG_VERSION := cosmic
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-cosmic
 #KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-11/bin/aarch64-linux-gnu-
 
 # GCC
