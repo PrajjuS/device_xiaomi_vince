16382eb9bdae9c1aa6c1d576168d289bdb556d60
Michael Bestas <mkbestas@lineageos.org>
2022-08-05 15:13:21 +0530

    vince: Use SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS
    Fixes:
      warning: BOARD_PLAT_PRIVATE_SEPOLICY_DIR has been deprecated.
               Use SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS instead.
    
    Change-Id: I563ce8a71626f2ebdebfd359e0d72935fd28998b
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -211,7 +211,7 @@ VENDOR_SECURITY_PATCH := 2021-07-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # SurfaceFlinger
