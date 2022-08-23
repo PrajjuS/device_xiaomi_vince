4b4088eb0e8ebd95e6a0ae5d3882fa66680e4a43
PrajjuS <theprajjus@gmail.com>
2022-08-23 17:07:32 +0530

    vince: Set TARGET_RECOVERY_DEVICE_MODULES := libinit_vince
    - Fixes:
        FAILED:
          build/make/core/main.mk:754: warning: Missing required dependencies:
          TARGET module recovery_deps requires non-existent TARGET module: libinit_msm8953
          build/make/core/main.mk:759: warning: Set BUILD_BROKEN_MISSING_REQUIRED_MODULES := true to bypass this check if this is intentional
          build/make/core/main.mk:760: error: Build failed.
    
    Change-Id: I6002e8ce659e31fb5ae30064ed0794326063dcb5
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,7 +171,7 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vintf/vendor.qti.esepowermanager@1.0-serv
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_vince
-TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
+TARGET_RECOVERY_DEVICE_MODULES := libinit_vince
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
