dd2cae7635b02647bfe2d602b670c531850e08b7
Dyneteve <sound0020@gmail.com>
2023-01-13 12:17:09 +0530

    vince: Enable support for call recording
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- aosp_vince.mk
+++ aosp_vince.mk
@@ -33,6 +33,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # PixelExperience specific flags
 TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_CALL_RECORDING := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions
