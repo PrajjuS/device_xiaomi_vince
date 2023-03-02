a4297be77047fcea6118a76a06a3982421e2e1a7
GeoPD <geoemmanuelpd2001@gmail.com>
2023-03-02 08:26:17 +0530

    vince: Drop shim for libdpmframework.so
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- device.mk
+++ device.mk
@@ -411,8 +411,7 @@ PRODUCT_COPY_FILES += \
 # Shims
 PRODUCT_PACKAGES += \
     libui_shim \
-    libui_shim.vendor:32 \
-    libdpmframework_shim
+    libui_shim.vendor:32
 
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
