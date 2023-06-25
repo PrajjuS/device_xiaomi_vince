be7e96265f40a45c9664a0c5eeb85684dd0d0e30
PrajjuS <theprajjus@gmail.com>
2023-06-25 07:48:34 +0530

    Brrrr 2
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone --depth=1 https://github.com/anandhan07/android_vendor_xiaomi_vince -b pixelexperience-13.0 vendor/xiaomi/vince
+git clone --depth=1 https://github.com/aospa-vince/android_kernel_xiaomi_vince -b topaz kernel/xiaomi/vince
+git clone --depth=1 https://github.com/iusmac/RevampedFMRadio -b qcom packages/apps/RevampedFMRadio
+git clone --depth=1 https://github.com/GrapheneOS/platform_external_Camera -b 13 external/Camera
+git clone --depth=1 https://gitlab.com/anandhan07/packages_apps_ViPER4AndroidFX.git -b master packages/apps/ViPER4AndroidFX
+git clone --depth=1 https://gitlab.com/anandhan07/aosp-clang.git -b 17 prebuilts/clang/host/linux-x86/clang-aosp
