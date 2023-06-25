88cba0fd97c1b3745e277d01ad8a6d5d425e5a83
PrajjuS <theprajjus@gmail.com>
2023-06-25 08:56:14 +0530

    Brrrr 3
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- removepackages/Android.mk
+++ removepackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Aperture Chrome
+LOCAL_OVERRIDES_PACKAGES := Aperture Drive PrebuiltGmail YouTube Videos RecorderPrebuilt TipsPrebuilt arcore SafetyHubPrebuilt AndroidAutoStubPrebuilt Camera2 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
