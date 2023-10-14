acd6050127914175ac83d30c6ccc563fb98a5f27
PrajjuS <theprajjus@gmail.com>
2023-10-15 05:11:38 +0530

    vince: vendorsetup: Track display and audio hals from my git
    Change-Id: Ie894e3c259c04a6f2e692939ee9caad5ebcff8ce
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,8 +8,8 @@ rm -rf hardware/qcom-caf/wlan
 rm -rf hardware/qcom-caf/msm8996
 
 git clone --depth=1 https://github.com/adnan-44/hardware_qcom-caf_wlan -b twelve hardware/qcom-caf/wlan
-git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996_audio -b twelve-LA.UM.9.6.2.r1-04100-89xx.0 hardware/qcom-caf/msm8996/audio
-git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996_display -b twelve-LA.UM.9.6.2.r1-04100-89xx.0 hardware/qcom-caf/msm8996/display
+git clone --depth=1 https://github.com/PrajjuS/hardware_qcom-caf_msm8996_audio -b twelve-LA.UM.9.6.2.r1-04100-89xx.0 hardware/qcom-caf/msm8996/audio
 git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996_media -b twelve-LA.UM.9.6.2.r1-04100-89xx.0 hardware/qcom-caf/msm8996/media
+git clone --depth=1 https://github.com/PrajjuS/hardware_qcom-caf_msm8996_display -b twelve-LA.UM.9.6.2.r1-04100-89xx.0 hardware/qcom-caf/msm8996/display
 
 git clone --depth=1 https://gitlab.com/GhostMaster69-dev/cosmic-clang -b master prebuilts/clang/host/linux-x86/clang-cosmic
