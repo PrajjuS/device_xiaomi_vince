8dee74c11d31f0c8ba3eed180724b92e5bdc45d5
PrajjuS <theprajjus@gmail.com>
2023-10-14 12:58:08 +0530

    vince: vendorsetup: Switch to Cygnus kernel
    - Also track aosp clang
    
    Change-Id: Ideb9b31cc1c6d9b31c0d3152a8b4caea4cc19487
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ git clone --depth=1 https://github.com/PrajjuS/device_xiaomi_vince -b elixir-14 
 
 git clone --depth=1 https://github.com/PrajjuS/vendor_xiaomi_vince -b elixir-14 vendor/xiaomi/vince
 
-git clone --depth=1 https://github.com/GhostMaster69-dev/android_kernel_xiaomi_vince -b master kernel/xiaomi/vince
+git clone --depth=1 https://github.com/anandhan07/kernel_xiaomi_vince -b 14-wip kernel/xiaomi/vince
 
 rm -rf hardware/qcom-caf/wlan
 rm -rf hardware/qcom-caf/msm8996
@@ -12,4 +12,4 @@ git clone --depth=1 https://github.com/PrajjuS/hardware_qcom-caf_msm8996_audio -
 git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996_media -b twelve-LA.UM.9.6.2.r1-04100-89xx.0 hardware/qcom-caf/msm8996/media
 git clone --depth=1 https://github.com/PrajjuS/hardware_qcom-caf_msm8996_display -b twelve-LA.UM.9.6.2.r1-04100-89xx.0 hardware/qcom-caf/msm8996/display
 
-git clone --depth=1 https://gitlab.com/GhostMaster69-dev/cosmic-clang -b master prebuilts/clang/host/linux-x86/clang-cosmic
+git clone --depth=1 https://gitlab.com/anandhan07/aosp-clang.git -b 17 prebuilts/clang/host/linux-x86/clang-aosp
