de68709034c957e299eae3c03662d7621983eca1
Prajwal <theprajjus@gmail.com>
2023-10-27 04:52:58 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ git clone --depth=1 https://github.com/PrajjuS/device_xiaomi_vince -b elixir-14 
 
 git clone --depth=1 https://github.com/PrajjuS/vendor_xiaomi_vince -b elixir-14 vendor/xiaomi/vince
 
-git clone --depth=1 https://github.com/anandhan07/kernel_xiaomi_vince -b 14-wip kernel/xiaomi/vince
+git clone --depth=1 https://github.com/anandhan07/kernel_xiaomi_vince -b 14 kernel/xiaomi/vince
 
 rm -rf hardware/qcom-caf/wlan
 rm -rf hardware/qcom-caf/msm8996
